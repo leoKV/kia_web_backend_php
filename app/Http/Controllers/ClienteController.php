05e81c7f04a192a70245306307e4e1a1161c81c9
leoKV <kevorvs@gmail.com>
2024-10-11 15:20:07 -0600

    Login fixed

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -41,7 +41,6 @@ class ClienteController extends Controller
                 // Guardar el id del cliente en la variable
                 $clienteId = $resultado[0]->cliente_id;
 
-                // Guardar el id del cliente en la caché por 60 minutos
                 Cache::put($cacheKey, $clienteId, 60);
             }
 
@@ -57,40 +56,6 @@ class ClienteController extends Controller
         }
     }
 
-    // public function getPedidosByClienteId(Request $request)
-    // {
-    //     try {
-    //         $clienteId = $request->query('clienteId');
-    //         // Validar que el clienteId no sea nulo
-    //         if (!$clienteId) {
-    //             return response()->json(['message' => 'El ID del cliente es requerido.'], 400);
-    //         }
-    //         $cacheKey = "cliente_id_{$clienteId}";
-    //         // Intentar obtener los pedidos desde la caché
-    //         $pedidosCliente = Cache::get($cacheKey);
-    //         // Si no están en caché, hacer la consulta
-    //         if (!$pedidosCliente) {
-    //             $pedidosCliente = DB::select('SELECT * FROM sps_cliente_pedidos(?)', [$clienteId]);
-    //             // Decodificar el JSON en el campo canciones antes de almacenar en caché
-    //             foreach ($pedidosCliente as &$pedido) {
-    //                 $pedido->canciones = json_decode($pedido->canciones);
-    //             }
-    //             // Verificar que haya resultados antes de cachar
-    //             if (!empty($pedidosCliente)) {
-    //                 Cache::put($cacheKey, $pedidosCliente, 60);
-    //             } else {
-    //                 return response()->json(['message' => 'Pedidos no encontrados.'], 404);
-    //             }
-    //         }
-    //         // Retornar la lista de pedidos
-    //         return response()->json($pedidosCliente);
-
-    //     } catch (\Exception $e) {
-    //         Log::error('Error al obtener los pedidos del cliente: ' . $e->getMessage());
-    //         return response()->json(['error' => 'Error interno del servidor'], 500);
-    //     }
-    // }
-
     public function getPedidosByClienteId(Request $request)
     {
         try {
@@ -101,7 +66,7 @@ class ClienteController extends Controller
                 return response()->json(['message' => 'El ID del cliente es requerido.'], 400);
             }
 
-            $perPage = $request->input('per_page', 1);
+            $perPage = $request->input('per_page', 5);
             $page = $request->input('page', 1);
             $offset = ($page - 1) * $perPage;
 
@@ -110,7 +75,7 @@ class ClienteController extends Controller
 
             if ($total === null) {
                 $total = DB::selectOne('SELECT COUNT(*) AS count FROM sps_cliente_pedidos(?) AS c', [$clienteId])->count;
-                Cache::put($totalKey, $total, 60);
+                Cache::put($totalKey, $total);
             }
 
             $pedidosKey = "pedidos_cliente_" . md5($clienteId) . "_page_$page";
